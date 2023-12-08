191751b4d46d7d2f0278e10ec4576fb6c5bf1908
Ryan Dundun <rdundun3@gmail.com>
2023-12-07 18:31:41 -0600

    first version 12/7/23

---
+++ src/Components/Contact.js
@@ -0,0 +1,199 @@
+import React, { useState } from "react";
+import { Fade, Slide } from "react-reveal";
+
+const Contact = ({ data }) => {
+  const [loading, setLoading] = useState(false);
+  const [messageSent, setMessageSent] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validateForm = (formData) => {
+    if (!formData.name.trim()) return "Name is required.";
+    if (!formData.email.trim()) return "Email is required.";
+    if (!formData.subject.trim()) return "Subject is required.";
+    if (!formData.message.trim()) return "Message is required.";
+    return "";
+  };
+
+  const handleSubmit = async (e) => {
+    console.log('you clicked submit')
+    e.preventDefault();
+    setLoading(true);
+    setMessageSent(false);
+    setErrorMessage('');
+
+    // Assuming you want to send the form data as JSON
+    const formData = {
+      name: e.target.contactName.value,
+      email: e.target.contactEmail.value,
+      subject: e.target.contactSubject.value,
+      message: e.target.contactMessage.value
+    };
+
+    const validationError = validateForm(formData);
+    if (validationError) {
+      setErrorMessage(validationError);
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch('https://l9gyalwwmd.execute-api.us-east-2.amazonaws.com/contactStage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const data = await response.json();
+      console.log("api output:")
+      console.log(data)
+      if (data === 'success') {
+        console.log("YEP")
+        setMessageSent(true);
+      } else {
+        setErrorMessage('An error occurred.');
+      }
+    } catch (error) {
+      setErrorMessage('Failed to send message: ' + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  if (!data) return null;
+
+  const { name, address, phone, email, contactmessage } = data;
+  const { street, city, state, zip } = address;
+
+  const handleChange = (e) => {
+    // Your change handling logic here
+  };
+
+  
+
+  return (
+    <section id="contact">
+      <Fade bottom duration={1000}>
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
+
+          <div className="ten columns">
+            <p className="lead">{contactmessage}</p>
+          </div>
+        </div>
+      </Fade>
+
+      <div className="row">
+        <Slide left duration={1000}>
+          <div className="eight columns">
+            <form action="" method="post" id="contactForm" name="contactForm" onSubmit={handleSubmit}>
+              <fieldset>
+                <div>
+                  <label htmlFor="contactName">
+                    Name <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactName"
+                    name="contactName"
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactEmail">
+                    Email <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactEmail"
+                    name="contactEmail"
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactSubject">Subject<span className="required">*</span></label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactSubject"
+                    name="contactSubject"
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactMessage">
+                    Message <span className="required">*</span>
+                  </label>
+                  <textarea
+                    cols="50"
+                    rows="15"
+                    id="contactMessage"
+                    name="contactMessage"
+                  ></textarea>
+                </div>
+
+                <div>
+                  <button className="submit" type="submit" disabled={loading} >Submit</button>
+                  <span id="image-loader" style={{ display: loading ? 'block' : 'none' }}>
+                    <img alt="Loading..." src="images/loader.gif" />
+                  </span>
+                </div>
+              </fieldset>
+            </form>
+            {loading && <p style={{ fontSize: '20px', color: 'white' }}>Loading...</p>}
+{messageSent && <p style={{ fontSize: '20px', color: 'white' }}>Your message was sent, thank you!</p>}
+{errorMessage && <p style={{ fontSize: '20px', color: 'white' }}>{errorMessage}</p>}
+
+
+          </div>
+        </Slide>
+
+        <Slide right duration={1000}>
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Contact</h4>
+              <p className="address">
+                {name}
+                <br />
+                <span>{phone}</span>
+                <br />
+                <span>{email}</span>
+              </p>
+            </div>
+
+            <div className="widget widget_tweets">
+              <h4 className="widget-title">Connect With Me</h4>
+              <ul id="twitter">
+                <li>
+                  <span>
+                  Interested in my work or have a question? Don't hesitate to reach out!
+                  </span>
+                </li>
+                <h4 className="widget-title">Fun Fact</h4>
+                <li>
+                  <span>
+                    This form's backend is powered by an API I developed using AWS Lambda and API Gateway. Additionally, both this website and the FourKube site were independently coded by me using React Javascript and are hosted on AWS S3.
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </aside>
+        </Slide>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/Components/Header.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
+import Fade from "react-reveal";
+
+class Header extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const project = this.props.data.project;
+    const github = this.props.data.github;
+    const name = this.props.data.name;
+    const description = this.props.data.description;
+    const description2 = this.props.data.description2;
+    const scrollToPortfolio = (e) => {
+      e.preventDefault(); // Prevent default anchor click behavior
+      const portfolioSection = document.getElementById('portfolio');
+      if (portfolioSection) {
+          portfolioSection.scrollIntoView({ behavior: 'smooth' });
+      }
+  };
+
+    return (
+      <header id="home">
+        <ParticlesBg type="random" bg={true} />
+
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <Fade bottom>
+              <h1 className="responsive-headline">{name}</h1>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}.</h3>
+            </Fade>
+            <br></br>
+            <Fade bottom duration={1200}>
+              <h3>{description2}.</h3>
+            </Fade>
+            <hr />
+            <Fade bottom duration={2000}>
+              <ul className="social">
+                <a href="#portfolio" className="button btn project-btn" onClick={scrollToPortfolio}>
+                  <i className="fa fa-book"></i>My Projects
+                </a>
+                <a href="https://github.com/ryandundun" className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            </Fade>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
